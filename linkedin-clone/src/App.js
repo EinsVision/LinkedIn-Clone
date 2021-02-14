dcae27b1e8c4de0d769b05e131cf5bd96bf610f0
jungchangdae <einsvision@gmail.com>
2021-02-14 10:46:36 +0900

    LinkedIn clone start

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
 import './App.css';
+import Header from './Header';
+
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build a LinkedIn Clone</h1>
+
+      <Header />
+
+      {/* App Body */}
+        {/* Siderbar  */}
+        {/* Feed */}
+        {/* Widgets */}
     </div>
   );
 }

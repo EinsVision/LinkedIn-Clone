ce9a8a9295d4c83b9050af1e67076f12f3a0ebaf
jungchangdae <einsvision@gmail.com>
2021-02-14 19:47:05 +0900

    posting on Linked In

--- linkedin-clone/src/Feed.js
+++ linkedin-clone/src/Feed.js
@@ -8,29 +8,36 @@ import EventNoteIcon from '@material-ui/icons/EventNote';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 import Post from './Post'
 import { db } from './firebase';
+import firebase from 'firebase';
 
 function Feed() {
+    const [input, setInput] = useState("");
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        db.collection('posts').onSnapshot(snapshot => (
-            setPosts(snapshot.docs.map(doc => (
+        db.collection("posts").onSnapshot((snapshot) => 
+            setPosts(snapshot.docs.map((doc) => (
                 {
                     id: doc.id,
-                    data: doc.data()
+                    data: doc.data(),
                 }
             )))
-        ))
-    }, [])
+        );
+    }, []);
 
-    const sendPost = e => {
+    const sendPost = (e) => {
         e.preventDefault();
-        db.collection('posts').add({
+        
+        db.collection("posts").add({
             name: 'changdae jung',
             description: 'this is a test',
-            message: ''
-        })
-    }
+            message: input,
+            photoUrl: '',
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+
+        setInput("");
+    };
     
 
     return (
@@ -39,7 +46,7 @@ function Feed() {
                 <div className='feed__input'>
                     <CreateIcon />
                     <form>
-                        <input type='text'/>
+                        <input value={input} onChange={ e => setInput(e.target.value)} type='text'/>
                         <button onClick={sendPost} type='submit'>Send</button>
                     </form>
                 </div>
@@ -51,9 +58,15 @@ function Feed() {
                 </div>
             </div>
             {/* Posts */}
-            {posts.map((post)=>{
-                <Post />
-            })}
+            {posts.map(({ id, data: { name, description, message, photoUrl } })=>(
+                <Post
+                    key={id}
+                    name={name}
+                    description={description}
+                    message={message}
+                    photoUrl={photoUrl} 
+                />
+            ))}
             <Post name='changdae jung' description='this is a test' message='Wow'/>
         </div>
     )
